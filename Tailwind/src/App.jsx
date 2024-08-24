eccf4267309b8c547b1c7dd8ded9f05ed25a0d06
rishav_learnerml <rishavbio1@gmail.com>
2024-08-24 18:29:48 +0530

    grid-system

--- Tailwind/src/App.jsx
+++ Tailwind/src/App.jsx
@@ -8,12 +8,19 @@ function App() {
         <div style={{ background: "red" }}>Hi</div>
         <div style={{ background: "green" }}>Hi</div>
         <div style={{ background: "yellow" }}>Hi</div>
-        
-      </div> */}
+      </div>
+      */}
+      {/* 
       <div className="flex justify-between">
         <div className="bg-red-300">Hi</div>
         <div className="bg-blue-300">Hi</div>
         <div className="bg-green-300">Hi</div>
+      </div> 
+      */}
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-2 p-2">
+        <div className="bg-red-300 col-span-1">Hi</div>
+        <div className="bg-blue-300 col-span-1">Hi</div>
+        <div className="bg-green-300 col-span-1">Hi</div>
       </div>
     </>
   );
